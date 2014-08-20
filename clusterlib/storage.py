7743ef1ca2a65ee4eb4c3c3f02a117d3ecc889a7
Arnaud Joly <arnaud.v.joly@gmail.com>
2014-08-20 12:41:08 +0200

    Add a simple distributed storage based on sqlite3

---
+++ clusterlib/storage.py
@@ -0,0 +1,73 @@
+"""
+This module allows to load and store values using a simple sqlite database.
+"""
+
+import os
+import sqlite3
+import pickle
+
+__all__ = [
+    "sqlite3_loads",
+    "sqlite3_dumps",
+]
+
+
+# sqlite3 ---------------------------------------------------------------------
+
+def sqlite3_loads(fname, key, timeout=7200.0):
+    """
+
+    Parameters
+    ----------
+    fname : str
+        path to the sqlite database
+
+    key : str or list of str
+
+    Returns
+    -------
+    value : object or list of object
+        Return the object or list of objects associated to each key.
+        None is return if the key is missing.
+
+    """
+    if isinstance(key, str):
+        key = [key]
+
+    if not os.path.exists(fname):
+        raise ValueError("No sqlite database at %s" % fname)
+
+    with sqlite3.connect(fname, timeout=timeout) as connection:
+        cursor = connection.cursor()
+        out = []
+        for k in key:
+            cursor.execute("SELECT value FROM dict where key = ?", (k,))
+            value = cursor.fetchone()
+            if value is None:
+                out.append(value)
+            else:
+                out.append(value[0])
+
+    out = [None if value is None else pickle.loads(bytes(value))
+           for value in out]
+
+    # Ravel if needed
+    if len(key) == 1:
+        out = out[0]
+
+    return out
+
+
+def sqlite3_dumps(fname, key, value, timeout=7200.0):
+    """Dumpy value at key in the sqlite3 database"""
+    value = pickle.dumps(value, protocol=pickle.HIGHEST_PROTOCOL)
+
+    connection = sqlite3.connect(fname, timeout=timeout)
+    with connection:
+        # Create table if needed
+        connection.execute("""CREATE TABLE IF NOT EXISTS dict
+                              (key TEXT PRIMARY KEY, value BLOB)""")
+
+        # Add a new key
+        connection.execute("INSERT INTO dict(key, value) VALUES (?, ?)",
+                           (key, sqlite3.Binary(value)))
---
+++ clusterlib/tests/test_storage.py
@@ -0,0 +1,32 @@
+from tempfile import NamedTemporaryFile
+
+from nose.tools import assert_equal
+
+from ..storage import sqlite3_loads
+from ..storage import sqlite3_dumps
+
+
+def test_sqlite3_storage():
+    with NamedTemporaryFile() as fhandle:
+        fname = fhandle.name
+
+        for i in range(5):
+            sqlite3_dumps(fname, str(i), i)
+
+        for i in range(5):
+            assert_equal(sqlite3_loads(fname, str(i)), i)
+
+        # List of key
+        assert_equal(sqlite3_loads(fname, ["1", "3"]), [1, 3])
+
+        # Missing in the db
+        assert_equal(sqlite3_loads(fname, "unkonwn"), None)
+        assert_equal(sqlite3_loads(fname, ["0", "unkonwn"]), [0, None])
+
+        # Complex object
+        complex_object = {
+            "tuple": (3, 2, "a"),
+            "set": set([2, 3]),
+        }
+        sqlite3_dumps(fname, "complex", complex_object)
+        assert_equal(sqlite3_loads(fname, "complex"), complex_object)

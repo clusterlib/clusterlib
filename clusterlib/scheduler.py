42727d624d8e935634e2d79eaccc2c6b178f0121
Arnaud Joly <arnaud.v.joly@gmail.com>
2014-08-21 14:06:57 +0200

    Add a submission query writer

--- clusterlib/scheduler.py
+++ clusterlib/scheduler.py
@@ -13,6 +13,7 @@ from xml.etree import ElementTree
 
 __all__ = [
     "queued_or_running_jobs",
+    "submit"
 ]
 
 def _sge_queued_or_running_jobs(warn=False):
@@ -45,3 +46,97 @@ def queued_or_running_jobs():
         out.extend(queued_or_running())
 
     return out
+
+
+_SGE_TEMPLATE = {
+    "job_name": '-N "%s"',
+    "memory": "-l h_vmem=%sM",
+    "time": "-l h_rt=%s",
+    "email": "-M %s",
+    "email_options": "-m %s",
+    "log_directory": "-o %s/$JOB_NAME.$JOB_ID",
+}
+
+_SLURM_TEMPLATE = {
+    "job_name": '--job-name=%s',
+    "memory": "--mem=%s",
+    "time": "--time=%s",
+    "email": "--mail-user=%s",
+    "email_options": "--mail-type=%s",
+    "log_directory": "-o %s/%j",
+}
+
+_TEMPLATE = {
+    "sge": _SGE_TEMPLATE,
+    "slurm": _SLURM_TEMPLATE
+}
+
+_LAUNCHER = {
+    "sge": "qsub",
+    "slurm": "sbatch",
+}
+
+def submit(job_command, job_name="job", time="24:00:00", memory=4000,
+           email=None, email_options=None, log_directory=None, backend="sge"):
+    """Write the submission query (without script)
+
+    Parameters
+    ----------
+    job_command : str,
+        command associated to the job, e.g. 'python main.py'.
+
+    job_name : str, optional
+        name of the job.
+
+    time : str, optional
+        maximum time format "HH:MM:SS".
+
+    memory : str, optional
+        maximum virtual memory in mega-bytes
+
+    email_address : str, optional
+        email where job information are sent.
+
+    email_options : str, optional
+        Format char from beas (begin,end,abort,stop).
+
+    log_directory : str, optional
+        Specify the log directory
+
+    backend : 'sge' or 'slurm'
+        Backend where the job will be submitted
+
+    Returns
+    -------
+    submission_query : str,
+        Return the submission query in the appropriate format
+
+    """
+    if backend in _TEMPLATE:
+        launcher = _LAUNCHER[backend]
+        template = _TEMPLATE[backend]
+    else:
+        raise ValueError("Unknown backend %s expected any of %s"
+                         % (backend, "{%s}" % ",".join(_TEMPLATE)))
+
+    job_options = [
+        template["job_name"] % job_name,
+        template["time"] % time,
+        template["memory"] % memory,
+    ]
+
+    if email:
+        job_options.append(template["email"] % email)
+
+    if email_options:
+        job_options.append(template["email_options"] % email_options)
+
+    if log_directory:
+        job_options.append(template["log_directory"] % log_directory)
+
+    # Using echo job_commands | launcher job_options allows to avoid creating
+    # a script file. The script is indeed created on the flight.
+    command = ("echo '%s' | %s %s"
+               % (job_command, launcher, " ".join(job_options)))
+
+    return command
--- clusterlib/tests/test_scheduler.py
+++ clusterlib/tests/test_scheduler.py
@@ -1,12 +1,35 @@
 # Authors: Arnaud Joly
 #
 # License: BSD 3 clause
+from nose.tools import assert_equal
+from nose.tools import assert_raises
 
 from ..scheduler import queued_or_running_jobs
+from ..scheduler import submit
 
-
-# XXX :  need a better way to test those functions
 
 def test_smoke_test():
+    # XXX :  need a better way to test those functions
     queued_or_running_jobs()
 
+
+def test_submit():
+    assert_equal(submit(job_command="python main.py", backend="sge"),
+        'echo \'python main.py\' | qsub -N "job" -l h_rt=24:00:00 '
+        '-l h_vmem=4000M')
+
+    assert_equal(submit(job_command="python main.py", backend="slurm"),
+         "echo 'python main.py' | sbatch --job-name=job --time=24:00:00 "
+         "--mem=4000")
+
+    assert_equal(submit(job_command="python main.py", email="test@test.com",
+                        email_options="beas"),
+         'echo \'python main.py\' | qsub -N "job" -l h_rt=24:00:00 '
+         '-l h_vmem=4000M -M test@test.com -m beas')
+
+    assert_equal(submit(job_command="python main.py",
+                        log_directory="path/test"),
+         'echo \'python main.py\' | qsub -N "job" -l h_rt=24:00:00 '
+         '-l h_vmem=4000M -o path/test/$JOB_NAME.$JOB_ID')
+
+    assert_raises(ValueError, submit, job_command="", backend="unknown")
